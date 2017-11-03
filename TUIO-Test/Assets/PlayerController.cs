b43ace8ce1fdbd0122e30a4231991b05c55613ad
zevau <cevau@posteo.de>
2017-11-03 16:37:27 +0100

    Test Level version 1

---
+++ TUIO-Test/Assets/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    
+    private Rigidbody rb;
+    
+    public float velocityFactor = 0.1f;
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        rb = player.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float angle = gameObject.transform.eulerAngles.z;
+        float y = rb.velocity.y;
+        Debug.Log("z: " + angle);
+        if (angle > 180)
+        {
+            rb.velocity = new Vector3((360 - angle) * velocityFactor, y, 0);
+        }
+        else
+        {
+            rb.velocity = new Vector3(-angle * velocityFactor, y, 0);
+        }
+    }
+}
