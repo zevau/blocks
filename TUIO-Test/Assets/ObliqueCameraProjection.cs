9d1f2afde1a7091a694dd35342cc32e681cc8125
zevau <cevau@posteo.de>
2017-11-08 19:22:20 +0100

    Oblique Orthographic Projection
    fine tuning still in progress

---
+++ TUIO-Test/Assets/ObliqueCameraProjection.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObliqueCameraProjection : MonoBehaviour {
+
+    public float xOblique = 0.01f;
+    public float yOblique = -0.01f;
+    void Start()
+    {
+        SetObliqueness(xOblique, yOblique);
+
+    }
+    void SetObliqueness (float horizObl, float vertObl)
+    {
+        Matrix4x4 matrix = Camera.main.projectionMatrix;
+        matrix[0, 2] = horizObl;
+        matrix[1, 2] = vertObl;
+        Camera.main.projectionMatrix = matrix;
+    }
+}
