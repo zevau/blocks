591a255206e7dbaeee633da21c0293a01b50b8eb
Nana <tietje.hh@gmail.com>
2017-12-07 14:49:10 +0100

    add TestGame

---
+++ Assets/Scripts/ReloadCube.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReloadCube : MonoBehaviour {
+
+	public FiducialController Fiducial;
+	public GameManager Manager;
+
+	bool FirstTime = true;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (FirstTime && Fiducial.IsVisible) {
+			FirstTime = false;
+			Manager.Reload ();
+		}
+	}
+}
